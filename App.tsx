e2bd3d112150dc293651178474769c6260ec076d
Dashiell Boyles <93666673+Dash1704@users.noreply.github.com>
2023-02-28 08:15:37 +0000

    basic maths working

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import { useState } from 'react';
 
-export function App() {
-  const [salary, setSalary] = useState<any | undefined>(0)
-  const [time, setTime] = useState<any | undefined>(0)
+function App() {
+  const [salary, setSalary] = useState<number | string>("")
+  const [weekHours, setWeekHours] = useState<number | string>("")
+  const [time, setTime] = useState<number | string>("")
+  const [showValue, setShowValue] = useState<boolean>(false)
 
-  console.log(salary)
-  console.log(time)
-
-  const calculator = ((salary / 525600) * time)
+  const calculator = (+salary / (+weekHours * 52) / 60 * +time)
+  const convert = calculator.toString()
+  const earnings = convert.substring(0, 4)
 
   return (
     <View style={styles.container}>
@@ -22,17 +22,24 @@ export function App() {
       </View>
 
       <View style={styles.moneyContainer}>
+        <Text style={styles.info}>Hours per week: </Text>
+        <TextInput style={styles.salaryBox} onChangeText={setWeekHours}>{weekHours}</TextInput>
+      </View>
+
+      <View style={styles.moneyContainer}>
         <Text style={styles.info}>Time on bog (mins): </Text>
         <TextInput style={styles.salaryBox} onChangeText={setTime}>{time}</TextInput>
       </View>
 
       <View style={styles.button}>
-        <Button title='Click to see earnings'></Button>
+        <Button title='Click to see earnings' onPress={() => setShowValue(true)}></Button>
+        {showValue ? <Text style={styles.info}>£{earnings}</Text> : null}
       </View>
 
-     
-
-
+      <View style={styles.button}>
+        <Button title='Reset Fields' onPress={() => setShowValue(true)}></Button>
+      </View>
+      
       {/* <View style={styles.button}>
         <Button title='start timer'></Button>
         <Button title='stop timer'></Button>
@@ -40,6 +47,8 @@ export function App() {
     </View>
   );
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
