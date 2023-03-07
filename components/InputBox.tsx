96dd25eb503822f71b4377d3726da8b6838e1245
Dashiell Boyles <93666673+Dash1704@users.noreply.github.com>
2023-03-07 09:34:57 +0000

    text align with box

--- components/InputBox.tsx
+++ components/InputBox.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 export function InputBox(props: any){
   return (
     <View style={styles.container}>
-      <Text style={styles.description}>{props.boxTitle}</Text>
+      <Text style={styles.titleText}>{props.boxTitle}</Text>
       <TextInput style={styles.boxContainer} onChangeText={props.onChangeText}></TextInput>
     </View>
   )
@@ -14,18 +14,18 @@ const styles = StyleSheet.create({
   container: {
     width: 313
   },
-  description: {
+  titleText: {
     fontSize: 14,
     fontWeight: "600",
     letterSpacing: -0.05,
     color: "#000000",
     lineHeight: 21,
     // fontFamily: 'Inter'
+    // justifyContent: 'flex-start'
   },
   boxContainer: {
     marginTop: 4,
     height: 54,
-    margin: 12,
     borderWidth: 1,
     padding: 10,
     borderRadius: 5,
